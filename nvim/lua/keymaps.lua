86a1a3f2ba06121538c02c503c39a9cd7697f912
Dominique <domi.github@gmail.com>
2025-02-07 13:11:33 +0100

    2025-02-07 13:11:33

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -33,3 +33,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Map 'jk' to escape in insert mode
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
